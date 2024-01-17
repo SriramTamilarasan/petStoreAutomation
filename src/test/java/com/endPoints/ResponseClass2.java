0e2eca958d34b005d7a5d1ed1d401a2129e7b2c1
srira <srira@Sriram>
2024-01-17 11:31:13 +0530

    I have created ResponseClass2 for our training purpose

---
+++ src/test/java/com/endPoints/ResponseClass2.java
@@ -0,0 +1,59 @@
+package com.endPoints;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import com.payload.UserPojoClass;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class ResponseClass2 {
+
+	public static Response createUser(UserPojoClass payload){
+		
+		Response res = given()
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.body(payload)
+		.when()
+			.post(RoutesClass.post_url);
+	
+		return res;
+	}
+	
+	public static Response getUser(String userName){
+		
+		Response res = given()
+			.pathParam("username", userName)
+		.when()
+			.get(RoutesClass.get_url);
+		
+		return res;
+	}
+	
+	public static Response updateUser(String userName, UserPojoClass payload){
+		
+		Response res = given()
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.pathParam("username", userName)
+			.body(payload)
+		.when()
+			.put(RoutesClass.put_url);
+		
+		return res;
+	}
+	
+	public static Response deleteUser(String userName){
+		
+		Response res = given()
+			.pathParam("username", userName)
+		.when()
+			.delete(RoutesClass.delete_url);
+		
+		return res;
+	}
+
+}
